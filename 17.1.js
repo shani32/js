61dfab6f869b353c8f3303cf4254bea4748bd341
shani barak <shanibrk@gmail.com>
2021-11-18 00:34:47 +0200

    17.1

--- 17.1.js
+++ 17.1.js
@@ -7,16 +7,18 @@ function someFunction(number) {
  return otherFunction;
 }
 var firstResult = someFunction(9);
+console.log(firstResult)
 var result = firstResult(2);
+console.log(result)
 
 //2
 var a = 1;
 function b2() {
- a = 10;
+a = 10;
  return;
  function a() { }
 }
-console.log(b2());
+console .log(b2());//return undefined. there is nothing to return
 console.log(a); //global scope=1 will print out the var
 //3
 let i;
@@ -24,5 +26,7 @@ for (i = 0; i < 3; i++) {
  const log = () => {
  console.log(i);
  }
- setTimeout(log, 100);//log will print 3 times 3 because it saves i and not the resault at time out.100 i think means nothing at this point
+//  setTimeout(log,100);//log will print 3 times 3 because it saves i and not the resault at time out.100 i think means nothing at this point
+setTimeout(log, 100);
+
 }
